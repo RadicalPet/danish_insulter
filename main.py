149b600bf1b8433ad15c96b2155d22f0d3f330bb
jagenau <sarah@radicalpet.net>
2022-09-01 02:11:14 +0200

    add website on root

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from typing import Union
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from insult import *
 
 
@@ -15,15 +18,29 @@ tags_metadata = [
 ]
 
 app = FastAPI(openapi_tags=tags_metadata)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Danish": "Insulter"}
+@app.get("/", response_class=HTMLResponse, tags=["default"])
+def read_root(request: Request, insult:bool = False, id:Union[str, None] = None, subject:str = False, unique:bool = False, alliteration:bool = False):
+
+    if insult:
+        if subject=="me":
+            subject=None
+        insult = Insult(id=id, subject=subject, unique=unique, alliteration=alliteration)
+        try:
+            error, id, insult = insult.get_insult()
+        except ListExhaustedException as e:
+            insult = Insult(id=None, subject=subject, unique=unique, alliteration=alliteration)
+            error, id, insult = insult.get_insult()
+        insult_result = insult
+    return templates.TemplateResponse("insult.html", {"request": request, "id": id, "subject": subject, "unique": unique, "insult_result": insult_result })
+
 
 
 @app.get("/nederen", tags=["nederen"])
-def read_item(
+def read_nederen(
     id: Union[str, None] =  Query(None, description="uuid to log the insult to - a uuid gets generated with every request without this parameter", ),
     subject: Union[str, None] =  Query(None, description='add subject for the insult (han/hen/hun/min chef etc.) - default is "du"', ),
     unique: bool = Query(False, description='keep insult values unique to uuid until shortest list exhausted', ),
